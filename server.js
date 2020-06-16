db4f50a0410c683121c869777f75cba9452736a7
Jatin Pandey <jatinpandey@Jatins-MacBook-Air.local>
2020-06-16 17:53:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express")
+app = express()
+const expressLayout=require('express-ejs-layouts')
+
+console.log("Working")
+
+app.use(expressLayout)
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
+app.set('view engine','ejs')
+
+var users=[]
+
+app.get('/',function(req,res){
+    res.render('index')
+})
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
+
+app.get('/register',function(req,res){
+    res.render('register')
+})
+
+app.post('/register',function(req,res){
+    var email=req.body.email
+    var name=req.body.name
+    var password=req.body.password
+    users.push({
+        "name":name,
+        "email":email,
+        "password":password
+    })
+    res.redirect("/login")
+    console.log(users)
+})
+
+app.post('/login',(req,res)=>{
+    u = users.find(user=> user.email==req.body.email)
+    if(u==null){
+        res.redirect('/login')
+    }
+    else if(u.password==req.body.password){
+        res.send('Shi Password')
+    }
+    else{
+        res.send('Galat Password')
+    }
+})
+
+app.listen(3000)
