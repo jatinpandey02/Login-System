7e1f5f2212da3812e047083fdf424a0fc0f8397e
Jatin Pandey <jatinpandey@Jatins-MacBook-Air.local>
2020-07-04 11:00:32 +0530

    added mongoose database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const expressLayout=require('express-ejs-layouts')
 const bcrypt = require('bcrypt')
+const User = require('/Users/jatinpandey/Desktop/Web Dev/login system/User.js')
+const mongoose = require('mongoose')
 
 console.log("Working")
 
@@ -10,7 +12,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
 app.set('view engine','ejs')
 
-var users=[]
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/',function(req,res){
     res.render('index')
@@ -28,26 +33,35 @@ app.post('/register',function(req,res){
     var email=req.body.email
     var name=req.body.name
     var password=bcrypt.hashSync(req.body.password,10)
-    users.push({
-        "name":name,
-        "email":email,
-        "password":password
+    User.findOne({email:email}).then(user=>{
+        if(user){
+            res.render('/register.ejs',{error:"User with email id exist"})
+        }
+        else{
+            const newUser = new User({
+                name,
+                email,
+                password
+            })
+            newUser.save()
+        }
     })
     res.redirect("/login")
     console.log(users)
 })
 
 app.post('/login',(req,res)=>{
-    u = users.find(user=> user.email==req.body.email)
-    if(u==null){
-        res.render('login.ejs',{error:"Id not found"})
-    }
-    else if(bcrypt.compareSync(req.body.password,u.password)){
-        res.send('You are in')
-    }
-    else{
-        res.render('login.ejs',{error:'Wrong Password'})
-    }
+    User.findOne({email:req.body.email}).then(user=>{
+        if(!user){
+            res.render('login.ejs',{error:"Id not found"})
+        }
+        else if(bcrypt.compareSync(req.body.password,user.password)){
+            res.send('You are in')
+        }
+        else{
+            res.render('login.ejs',{error:'Wrong Password'})
+        }
+    })
 })
 
 app.listen(3000)
